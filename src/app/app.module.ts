44fbf157757de264e687df287b9dbb0c71eea556
Navid Farahmand <NavidFarahmand75@gmail.com>
2018-10-11 17:15:50 +0330

    add primeng module file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PrimengModule } from './primeng.module'
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +9,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    PrimengModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
