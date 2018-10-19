0c42d045e9104e4641c4875543087c9ac5e9a15b
Navid Farahmand <NavidFarahmand75@gmail.com>
2018-10-19 20:17:43 +0330

    add users content

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -7,9 +7,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersComponent implements OnInit {
 
+  cols: any[];
+  users: any[];
+  tempUsers: any[];
+  loading: boolean;
+  totalRecords: number;
+
   constructor() { }
 
   ngOnInit() {
+    this.loading = true;
+    this.users = [];
+    this.cols = [
+      { field: 'name', header: 'نام', width: '60%' },
+      { field: 'username', header: 'نام کاربری', width: '40%' },
+    ];
+    this.tempUsers = [
+      { username: 'asdf', name: 'asdf', },
+      { username: 'asdf', name: 'asdf', },
+      { username: 'asdf', name: 'asdf', },
+      { username: 'asdf', name: 'asdf', },
+      { username: 'asdf', name: 'asdf', },
+      { username: 'asdf', name: 'asdf', },
+      { username: 'asdf', name: 'asdf', },
+      { username: 'asdf', name: 'asdf', },
+    ];
+    this.totalRecords = this.tempUsers.length;
   }
-
+  loadUsers(event) {
+    this.loading = true;
+    console.log(event.first);
+    console.log(event.rows);
+    setTimeout(() => {
+      if (this.tempUsers) {
+        this.users = this.tempUsers.slice(event.first, (event.first + event.rows));
+        this.loading = false;
+      }
+    }, 1000);
+  }
 }
