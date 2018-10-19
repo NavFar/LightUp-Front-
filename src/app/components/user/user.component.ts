118d34d994d0622f61fc1519c137fefc4c3635b6
Navid Farahmand <NavidFarahmand75@gmail.com>
2018-10-20 00:08:13 +0330

    add user content

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router, ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
-
-  constructor() { }
+  title: string;
+  id: any;
+  constructor(private route: ActivatedRoute, private router: Router, ) { }
 
   ngOnInit() {
+    this.title = "افزودن کاربر";
+    this.id = this.route.snapshot.paramMap.get('id');
+    if (!Number(this.id))
+      this.router.navigate(['/menu']);
+    console.log(this.id);
   }
 
 }
