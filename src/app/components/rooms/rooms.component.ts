e0a853a10c56a022a4397e9a9824597aa2dadc45
Navid Farahmand <NavidFarahmand75@gmail.com>
2018-10-19 19:50:05 +0330

    add rooms content

--- src/app/components/rooms/rooms.component.ts
+++ src/app/components/rooms/rooms.component.ts
@@ -6,10 +6,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./rooms.component.css']
 })
 export class RoomsComponent implements OnInit {
-
+  cols: any[];
+  rooms: any[];
+  tempRooms: any[];
+  loading: boolean;
+  totalRecords: number;
   constructor() { }
 
   ngOnInit() {
+    this.loading = true;
+    this.rooms = [];
+    this.cols = [
+      { field: 'name', header: 'نام' },
+    ];
+    this.tempRooms = [
+      { name: 'asdf', },
+      { name: 'asdf', },
+      { name: 'asdf', },
+      { name: 'asdf', },
+      { name: 'asdf', },
+      { name: 'asdf', },
+      { name: 'asdf', },
+      { name: 'asdf', },
+    ];
+    this.totalRecords = this.tempRooms.length;
   }
-
+  loadRooms(event) {
+    this.loading = true;
+    console.log(event.first);
+    console.log(event.rows);
+    setTimeout(() => {
+      if (this.tempRooms) {
+        this.rooms = this.tempRooms.slice(event.first, (event.first + event.rows));
+        this.loading = false;
+      }
+    }, 1000);
+  }
 }
