79cbc68896bae80313fca85b75b41eaf4133cecf
Navid Farahmand <NavidFarahmand75@gmail.com>
2018-10-19 20:06:48 +0330

    add pattern content

--- src/app/components/patterns/patterns.component.ts
+++ src/app/components/patterns/patterns.component.ts
@@ -6,10 +6,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./patterns.component.css']
 })
 export class PatternsComponent implements OnInit {
+  cols: any[];
+  patterns: any[];
+  tempPatterns: any[];
+  loading: boolean;
+  totalRecords: number;
 
   constructor() { }
 
   ngOnInit() {
-  }
+    this.loading = true;
+    this.patterns = [];
+    this.cols = [
+      { field: 'name', header: 'نام', width: '75%' },
+      { field: 'active', header: 'فعال', width: '25%' },
+    ];
+    this.tempPatterns = [
+      { name: 'asasdfasdfasdfasdfasdfasdfasfsddf', active: 'asdf', },
+      { name: 'asdf', active: 'asdf', },
+      { name: 'asdf', active: 'asdf', },
+      { name: 'asdf', active: 'asdf', },
+      { name: 'asdf', active: 'asdf', },
+      { name: 'asdf', active: 'asdf', },
+      { name: 'asdf', active: 'asdf', },
+      { name: 'asdf', active: 'asdf', },
+    ];
+    this.totalRecords = this.tempPatterns.length;
 
+  }
+  loadPatterns(event) {
+    this.loading = true;
+    console.log(event.first);
+    console.log(event.rows);
+    setTimeout(() => {
+      if (this.tempPatterns) {
+        this.patterns = this.tempPatterns.slice(event.first, (event.first + event.rows));
+        this.loading = false;
+      }
+    }, 1000);
+  }
 }
