9f77582a3a48441f3e90130a90995d552ae0dd3a
Navid Farahmand <NavidFarahmand75@gmail.com>
2018-10-21 18:29:47 +0330

    add pattern content

--- src/app/components/pattern/pattern.component.ts
+++ src/app/components/pattern/pattern.component.ts
@@ -6,10 +6,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pattern.component.css']
 })
 export class PatternComponent implements OnInit {
-
+  title: string;
+  dialogHeader: string;
+  showDialog: boolean;
+  rooms: any[];
   constructor() { }
 
   ngOnInit() {
-  }
+    this.showDialog = false;
+    this.title = "افزودن الگو";
+    this.dialogHeader = "انتخاب کنید.";
+    this.rooms = [
+      { name: "خواب", value: 0 },
+      { name: "خواب", value: 0 },
+      { name: "خواب", value: 0 },
+      { name: "خواب", value: 0 },
+      { name: "خواب", value: 0 },
+      { name: "خواب", value: 0 },
+      { name: "خواب", value: 0 },
+      { name: "خواب", value: 0 },
+      { name: "خواب", value: 0 },
+      { name: "خواب", value: 0 },
+      { name: "خواب", value: 0 },
+      { name: "خواب", value: 0 },
+      { name: "خواب", value: 0 },
 
+    ];
+  }
+  add() {
+    this.showDialog = true;
+  }
 }
